1a2378a1f5781f73a8a2513a565534d4f87009e8
Sung Won Chung <sungwonchung3@gmail.com>
2021-06-01 11:33:29 -0500

    update to delete+insert to allow dupes in target table

--- main-project/models/incremental_examples/deal_with_incrementing_pkey.sql
+++ main-project/models/incremental_examples/deal_with_incrementing_pkey.sql
@@ -2,12 +2,12 @@
     config(
         materialized='incremental',
         unique_key='emailid',
-        incremental_strategy='merge',
+        incremental_strategy='delete+insert',
     )
 }}
 
 -- primary_key_of_destination_table: this needs to be replaced by the actual name of the primary key
 -- If you do NOT replace this, this model will NOT successfully run as primary_key_of_destination_table does NOT exist in either table
 -- I'll assume emailid in this example is the unique key as this reproduces the exact scenario illustrated in the original email
-
+-- By nature of the using a sequential surrogate key to serve as your primary key, the incremental logic will not work properly because it won't be possible to update old rows
 select * from {{ ref('first_table') }}
