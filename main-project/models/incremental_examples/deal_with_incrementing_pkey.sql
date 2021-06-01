c56b56acaa90ae7bf7c0a25baa85a266ae91d891
Sung Won Chung <sungwonchung3@gmail.com>
2021-06-01 10:54:19 -0500

    reproduce the problem based on new unique key

--- main-project/models/incremental_examples/deal_with_incrementing_pkey.sql
+++ main-project/models/incremental_examples/deal_with_incrementing_pkey.sql
@@ -1,9 +1,13 @@
 {{
     config(
         materialized='incremental',
-        unique_key='primary_key_of_destination_table',
+        unique_key='emailid',
         incremental_strategy='merge',
     )
 }}
+
+-- primary_key_of_destination_table: this needs to be replaced by the actual name of the primary key
+-- If you do NOT replace this, this model will NOT successfully run as primary_key_of_destination_table does NOT exist in either table
+-- I'll assume emailid in this example is the unique key as this reproduces the exact scenario illustrated in the original email
 
 select * from {{ ref('first_table') }}
